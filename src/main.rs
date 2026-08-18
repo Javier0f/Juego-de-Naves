75071bb0a2a4a183c793f2b88879864df9ac1316
Javier0f <luckynd37@gmail.com>
2026-08-18 18:01:15 -0300

    juego de nave

--- src/main.rs
+++ src/main.rs
@@ -15,44 +15,42 @@ struct Position{
 struct Velocity(Vec2);
 
 #[derive(Debug)]
-struct EntityType(u8);
-#[derive(Debug)]
 struct Size(f32);
 #[derive(Debug)]
 struct Speed(f32);
 #[derive(Debug)]
 struct Rot(f32);
 #[derive(Debug)]
-struct Sides(f32);
+struct Sides(u8);
 
 #[macroquad::main("Nave")]
 async fn main() {
     let mut world = World::new();
 
-    let background = Color::new(0.9, 0.9, 0.9, 1.0);
+    let _background = Color::new(0.01, 0.01, 0.05, 1.0);
 
     world.spawn((
-        EntityType(0),
         Player,
         Position{x: screen_width() / 2.0, y: screen_height() / 2.0},
         Size(30.0),
-        Speed(3.0),
+        Speed(6.0),
         Velocity(Vec2::ZERO),
-        Rot(22.5),
+        Rot(0.0),
     ));
 
-    for _i in 0..15 {
+    
+    for _i in 0..30 {
+        let r = rand::gen_range(0.1, 0.9);
+        let g = rand::gen_range(0.1, 0.9);
+        let b = rand::gen_range(0.1, 0.9);
 
-        let r = 0.2;// rand::gen_range(0., 1.);
-        let g = 0.3;// rand::gen_range(0., 1.);
-        let b = 0.6;// rand::gen_range(0., 1.);
+        let color_asteroid = Color::new(r, g, b, 1.0);
 
-        let color = Color::new(r , g , b , 1.0);
 
-        let size_rand = rand::gen_range(55.0, 99.0);
+        let size_rand = rand::gen_range(25.0, 90.0);
         let rand_x = rand::gen_range(size_rand, 1920.0 - size_rand);
         let rand_y = rand::gen_range(size_rand, 1080.0 - size_rand);
-        let rand_sides = rand::gen_range(3.0, 15.0);
+        let rand_sides = rand::gen_range(5, 10);
         
         let rand_vel_x = rand::gen_range(-1.0, 1.0);
         let rand_vel_y = rand::gen_range(-1.0, 1.0);
@@ -62,14 +60,13 @@ async fn main() {
         let rand_rot =  rand::gen_range(30.0, 35.0);
 
         world.spawn((
-            EntityType(1),
             Asteriod,
             Size(size_rand),
             Sides(rand_sides),
             Position{x: rand_x, y: rand_y},
             Velocity(Vec2::new(rand_vel_x, rand_vel_y)),
             Speed(rand_speed),
-            color,
+            color_asteroid,
             Rot(rand_rot),
         ));
     }
@@ -82,30 +79,28 @@ async fn main() {
     retro_camera.render_target = Some(render_target.clone());
 
     let mut entities_collide: Vec<(Entity, Vec2, Vec2)> = Vec::with_capacity(26);
-    // let mut asteroid_2_collide: Vec<(Entity, EntityType, Position, Velocity, Size, Speed)> = Vec::with_capacity(26);
-    // let mut asteroid_2_collide: Vec<_> = Vec::with_capacity(26);
 
     loop{
         let dt = get_frame_time();
         set_camera(&retro_camera);
-        clear_background(background);
+        clear_background(_background);
 
         // <----- MOVEMENT AND COLLISION -----> //
 
 
-        {// <-------- COLICIONES DE ASTEROIDES --------> //
-            let mut query = world.query::<(Entity ,&EntityType, &Position, &Velocity, &Size, &Speed)>();
+        {// <-------- COLLISION --------> //
+            let mut query = world.query::<(Entity, &Position, &Velocity, &Size, &Speed)>();
             let asteroid_2_collide: Vec<_> = query.iter().collect::<Vec<_>>();
 
             for a in asteroid_2_collide.iter(){
                 for b in asteroid_2_collide.iter(){
                     
                     if a.0 != b.0{
-                        let vec_a = Vec2::new(a.2.x, a.2.y);
-                        let vec_b = Vec2::new(b.2.x, b.2.y);
+                        let vec_a = Vec2::new(a.1.x, a.1.y);
+                        let vec_b = Vec2::new(b.1.x, b.1.y);
 
                         let distance = vec_a.distance(vec_b);
-                        let suma_radios = a.4.0 + b.4.0;
+                        let suma_radios = a.3.0 + b.3.0;
 
                         if distance < suma_radios && distance > 0.0 {
                             let repulcion = (vec_a - vec_b).normalize();
@@ -126,6 +121,19 @@ async fn main() {
         for (enti,_type, pos, speed, vel, rot) in world.query_mut::<(Entity, &Player, &mut Position, &Speed, &mut Velocity, &mut Rot)> () {
             let mut dir = Vec2::ZERO;
             let rot_rad = rot.0.to_radians();
+            
+            if pos.x > 1920.0 + 30.0 {
+                pos.x = -30.0
+            }
+            if pos.x < -30.0{
+                pos.x = 1920.0 + 30.0
+            }
+            if pos.y > 1080.0 + 30.0 {
+                pos.y = -30.0
+            }
+            if pos.y < -30.0{
+                pos.y = 1080.0 + 30.0
+            }
 
             entities_collide.iter().for_each(|collide| {
                 if collide.0 == enti{
@@ -136,12 +144,10 @@ async fn main() {
             });
     
             if is_key_down(KeyCode::Up)   {
-                // dir.x += 1.0;
                 dir.x = rot_rad.sin();
                 dir.y = -rot_rad.cos();
             }
             if is_key_down(KeyCode::Down) {
-                // dir.x -= 1.0;
                 dir.x = -rot_rad.sin();
                 dir.y = rot_rad.cos();
             }
@@ -196,30 +202,48 @@ async fn main() {
         // <-------- RENDER --------> //
 
         for (_type, pos, size, rot) in world.query_mut::<(&Player, &Position, &Size, &Rot)>() {
-            let rot_sin = rot.0.to_radians().sin();
-            let rot_cos = rot.0.to_radians().cos();
+            let sen_a = rot.0.to_radians().sin();
+            let cos_a = rot.0.to_radians().cos();
 
-            let h = size.0 ;// / 2.;
-            let b = size.0 ;// / 3.0;
+            let h = size.0 ;
+            let b = size.0 * 1.3;
+
+            let vec_a = vec2(
+                pos.x + h * sen_a,
+                pos.y - h * cos_a
+            );
+
+            let vec_b = vec2(
+                pos.x - cos_a * b - sen_a * h, 
+                pos.y - sen_a * b + cos_a * h
+            );
+
+            let vec_c = vec2(
+                pos.x + cos_a * b - sen_a * h, 
+                pos.y + sen_a * b + cos_a * h
+            );
             
-            let vec1 = Vec2::new(
-                pos.x + h * rot_sin,
-                pos.y - h * rot_cos,
-            );
-            let vec2 = Vec2::new(
-                pos.x - rot_cos * b - rot_sin * h,
-                pos.y - rot_sin * b + rot_cos * h,
-            );
-            let vec3 = Vec2::new(
-                pos.x + rot_cos * b - rot_sin * h,
-                pos.y + rot_sin * b + rot_cos * h,
-            );
-            
-            draw_triangle(vec1, vec2, vec3, RED);
+            draw_poly_lines(pos.x, pos.y, 3, size.0 + 3.0, rot.0 + 30.0 , 14.0, BLUE);
+            draw_triangle(vec_a, vec_b, vec_c, SKYBLUE);
+            //draw_circle_lines(pos.x, pos.y, size.0, 1., BLACK); // area de colision
         }
 
-        for (entity ,_type, pos, size, sides, rot, color) in world.query_mut::<(Entity ,&Asteriod, &Position, &Size, &Sides, &mut Rot, &Color)>() {
-            draw_poly_lines(pos.x, pos.y, sides.0 as u8, size.0, rot.0, 8.5, *color)
+        for (_type, pos, size, sides, rot, color) in world.query_mut::<(&Asteriod, &Position, &Size, &Sides, &mut Rot, &Color)>() {
+            // let cos = rot.0.to_radians().cos();
+            // let sen = rot.0.to_radians().sin();
+
+            // let dis = size.0 / 2.5;
+
+            // //              ( d , d )
+            // let vec_a = vec2( pos.x + dis*cos - dis*sen , pos.y + dis*sen + dis*cos);
+
+            // //              ( d , -d )
+            // let vec_b = vec2(pos.x + dis*cos + dis*sen, pos.y + dis*sen - dis*cos);
+
+            // draw_circle_lines(vec_a.x, vec_a.y, 10.0, 4.5, *color);
+            // draw_circle_lines(vec_b.x, vec_b.y, 10.0, 4.5, *color);
+
+            draw_poly_lines(pos.x, pos.y, sides.0, size.0, rot.0, 8.5, *color)
         }
 
         set_default_camera();
@@ -239,6 +263,5 @@ async fn main() {
 
         entities_collide.clear();
         next_frame().await;
-        // return;
     }
 }
