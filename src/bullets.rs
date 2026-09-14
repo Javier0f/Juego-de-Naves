902d19a43311b0181712dec6f2ca57cdb16375f8
Javier0f <luckynd37@gmail.com>
2026-09-13 21:13:19 -0300

    reorganización de código parte 6.4

--- src/bullets.rs
+++ src/bullets.rs
@@ -12,43 +12,31 @@ use crate::components::*;
 const BULLET_LIFE: i8 = 120;
 const BULLET_SPEED: f32 = 450.0;
 const MAX_COOLDOWN: u8 = 20;
-const SPAWN_DISTANCE: f32 = 55.0;
+// const SPAWN_DISTANCE: f32 = 55.0;
 
 pub fn bullets(world: &mut World, dt: f32, cooldown: &mut u8){
-    let mut bullet = (
-        Bullet,
-        Position(Vec2::ZERO),
-        Direction(Vec2::ZERO),
-        Life(BULLET_LIFE),
-        Size(5.0),
-    );
 
     let mut bullets_despawn: Vec<Entity> = vec![];
 
     if is_key_down(KeyCode::A) && *cooldown == 0{
+        let mut last_position : (Vec2, f32) = (Vec2::ZERO, 0.0);
         world.query::<With<(&Position, &Rotation), &Player>>()
         .iter()
-        .for_each(|(pos, rot)|{
-            let rad = rot.0.to_radians();
+        .for_each(|(pos, rot)|{last_position = (pos.0, rot.0);});
 
-            let new_pos = vec2(
-                pos.0.x + rad.sin() * SPAWN_DISTANCE,
-                pos.0.y - rad.cos() * SPAWN_DISTANCE
-            );
-
-            bullet.1.0 = new_pos;
-            bullet.2.0.y = -rad.cos() + rand::gen_range(-0.1,0.1);
-            bullet.2.0.x = rad.sin() + rand::gen_range(-0.1,0.1);
-            bullet.2.0 = bullet.2.0.normalize();
-        });
-        world.spawn(bullet);
+        world.spawn((
+            Bullet,
+            Position(last_position.0),
+            Direction(vec2(
+                last_position.1.to_radians().sin() + rand::gen_range(-0.1,0.1),
+                -last_position.1.to_radians().cos() + rand::gen_range(-0.1,0.1)
+            )),
+            Life(BULLET_LIFE),
+        ));
         *cooldown = MAX_COOLDOWN;
     }
 
-    if *cooldown > 0{
-        *cooldown -= 1;
-    }
-
+    *cooldown = cooldown.saturating_sub(1);
 
     world.query_mut::<With<(Entity, &mut Position, &mut Direction, &mut Life), &Bullet>>()
     .into_iter()
@@ -58,38 +46,27 @@ pub fn bullets(world: &mut World, dt: f32, cooldown: &mut u8){
 
             life.0 -= 1;
             pos.0 += dir.0 * BULLET_SPEED * dt;
-        }
-
-        if life.0 == 0 {
+        }else{
             bullets_despawn.push(e);
         }
     });
 
-    if bullets_despawn.len() > 0 {
-        bullets_despawn.iter().for_each(|e|{
-            let _ =world.despawn(*e);
-        });
-        bullets_despawn.clear();
-    }
+    bullets_despawn.iter().for_each(|e|{ let _ = world.despawn(*e); });
 
-    let mut collision = [(Vec2::ZERO, 0.0);30];
-    let mut index: usize = 0;
+    let mut collision: Vec<(Vec2,f32)> = vec![];
 
     world.query::<With<(&Position, &Size), &Asteroid>>()
     .iter()
     .for_each(|(pos, size)| {
-        collision[index] = (pos.0,size.0);
-        index += 1;
+        collision.push((pos.0,size.0));
     });
 
     world.query_mut::<With<(&Position, &mut Life),&Bullet>>()
     .into_iter()
     .for_each(|(pos, life)|{
         collision.iter().for_each(|(pos_a, size)|{
-            // let dis = pos.0.distance(*pos_a);
-            if (pos.0.distance(*pos_a) - size) < 0.0{
-                life.0 = 0;
-            }
+            let is_hit = (pos.0.distance_squared(*pos_a) < size * size )as i8;
+            life.0 *= 1 - is_hit;
         });
     });
 
